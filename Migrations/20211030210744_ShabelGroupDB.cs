a2c7dfea9b4972f0b98bcd69936891439c1cd3bf
blizzyblake278 <magicguitar19@gmail.com>
2021-10-30 16:11:07 -0500

    added previous local db connection to ensure default values

---
+++ Migrations/20211030210744_ShabelGroupDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CapstoneProjectCustomerListWebApp.Migrations
+{
+    public partial class ShabelGroupDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
